041005721acbc4d29280fed9915b1c897eb57a54
Gregory Melikhov <gr.melikhov12@gmail.com>
2025-10-20 19:19:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
+        bot.send_message(message.chat.id, pokemon.show_ability())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
 
 bot.infinity_polling(none_stop=True)
-
