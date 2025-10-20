84a04407e078fca2b11993f5306dd21f0700f5df
Gregory Melikhov <gr.melikhov12@gmail.com>
2025-10-20 19:18:59 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -11,16 +11,27 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.ability = self.get_ability()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
-    # Метод для получения имени покемона через API
+    # Метод для получения способности покемона через API
+    def get_ability(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities']['ability'])
+        else:
+            return "Pikachu"
+        
+    # Метод для получения картинки покемона через API
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['sprites']['other']['home']['front_default'])
+            return (data['sprites']['other']['official-artwork']['front_default'])
         else:
             return "Pikachu"
         
@@ -39,6 +50,10 @@ class Pokemon:
     def info(self):
         return f"Имя твоего покеомона: {self.name}"
 
+    # Метод класса для получения способности
+    def show_ability(self):
+        return f"Способность твоего покемона: {self.ability}"
+    
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
